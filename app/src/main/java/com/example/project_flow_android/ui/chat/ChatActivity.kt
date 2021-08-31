b82b79cff8df15c1f6ccdaab87e6608ac1e887e1
Anbyeonghun <abh0920one@gmail.com>
2021-08-31 19:22:15 +0900

    [STYLE] Chat main ui

---
+++ app/src/main/java/com/example/project_flow_android/ui/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project_flow_android.ui.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project_flow_android.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
