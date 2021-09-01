842526c44d8a4ced3af099eeca3000352993acec
dlswer23 <lbj4298@naver.com>
2021-09-01 22:18:45 +0900

    [UPDATE] api

--- app/src/main/java/com/example/project_flow_android/network/ApiProvider.kt
+++ app/src/main/java/com/example/project_flow_android/network/ApiProvider.kt
@@ -1,4 +1,22 @@
 package com.example.project_flow_android.network
 
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiProvider {
+
+    private var instance : Retrofit? = null
+    private val gson = GsonBuilder().setLenient().create()
+
+    fun getInstnace() : Retrofit {
+        if(instance == null){
+            instance = Retrofit.Builder()
+                .baseUrl("http://localhost:3000")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return instance!!
+    }
+
 }
--- app/src/main/java/com/example/project_flow_android/network/ProjectFlowAPI.kt
+++ app/src/main/java/com/example/project_flow_android/network/ProjectFlowAPI.kt
@@ -1,4 +1,23 @@
 package com.example.project_flow_android.network
 
+import android.telecom.Call
+import com.example.project_flow_android.feature.LoginRequest
+import com.example.project_flow_android.feature.RegisterRequest
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ProjectFlowAPI {
+
+    //Register
+    @POST("/auth/join")
+    fun doRegister(@Body request:RegisterRequest):retrofit2.Call<Unit>
+
+    //Login
+    @POST("/auth/login")
+    fun doLogin(@Body request:LoginRequest):retrofit2.Call<Unit>
+
+
+
+
 }
