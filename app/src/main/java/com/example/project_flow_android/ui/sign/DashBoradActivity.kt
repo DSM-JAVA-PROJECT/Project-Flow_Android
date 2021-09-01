57645ef681e3e12c7506af4bc98731ba63431b2f
dlswer23 <lbj4298@naver.com>
2021-09-01 16:18:12 +0900

    [STYLE] all

--- app/src/main/java/com/example/project_flow_android/ui/sign/DashBoradActivity.kt
+++ app/src/main/java/com/example/project_flow_android/ui/sign/DashBoradActivity.kt
@@ -2,6 +2,7 @@ package com.example.project_flow_android.ui.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_flow_android.R
 
 class DashBoradActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
