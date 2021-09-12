a60cb9dbc724811717d1059bd77350cb39830f38
Anbyeonghun <abh0920one@gmail.com>
2021-09-12 14:48:29 +0900

    [FIX] Chat RV item ui

--- app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ChatFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ChatFragment.kt
@@ -6,9 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat.getSystemService
 import com.bumptech.glide.Glide
 import com.example.project_flow_android.R
 import com.example.project_flow_android.network.StompClient
