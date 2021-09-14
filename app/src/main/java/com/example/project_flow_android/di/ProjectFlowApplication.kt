88ab6da3f346c27a7f479d01c1c8b1d8de0a7ecb
dlswer23 <lbj4298@naver.com>
2021-09-14 11:52:54 +0900

    [UPDATE] do setting
    아직 완성하지 않았음 sign branch랑 병합하고 di

---
+++ app/src/main/java/com/example/project_flow_android/di/ProjectFlowApplication.kt
@@ -0,0 +1,20 @@
+package com.example.project_flow_android.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class ProjectFlowApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@ProjectFlowApplication)
+            modules(
+                listOf(
+                    mainModule,
+
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project_flow_android/di/module/SignModule.kt
@@ -0,0 +1,8 @@
+package com.example.project_flow_android.di.module
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val SignModule = module {
+  
+}
