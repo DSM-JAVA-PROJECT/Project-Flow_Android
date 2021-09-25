c0e86bf82e1d7dd37f629f86752e962b982ceb0c
dlswer23 <lbj4298@naver.com>
2021-09-25 22:33:48 +0900

    [UPDATE] Application add main module
    main module 추가

--- app/src/main/java/com/example/project_flow_android/di/ProjectFlowApplication.kt
+++ app/src/main/java/com/example/project_flow_android/di/ProjectFlowApplication.kt
@@ -1,6 +1,7 @@
 package com.example.project_flow_android.di
 
 import android.app.Application
+import com.example.project_flow_android.di.module.mainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,7 +13,8 @@ class ProjectFlowApplication : Application() {
             modules(
                 listOf(
                     loginModule,
-                    registerModule
+                    registerModule,
+                    mainModule
                 )
             )
         }
