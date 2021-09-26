074d3f8ff5ac0e1e2ebb67a4c10aa04eed85130d
Anbyeonghun <abh0920one@gmail.com>
2021-09-26 15:22:03 +0900

    [REMOVE] StompClient

--- app/src/main/java/com/example/project_flow_android/di/SocketModule.kt
+++
@@ -1,8 +0,0 @@
-package com.example.project_flow_android.di
-
-import com.example.project_flow_android.network.StompClient
-import org.koin.dsl.module
-
-val socketModule = module {
-    single { StompClient() }
-}
