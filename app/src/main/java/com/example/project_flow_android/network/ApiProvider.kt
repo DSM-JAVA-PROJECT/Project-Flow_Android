712947b89dffa68215c45fb9c8af53371874ef67
Anbyeonghun <abh0920one@gmail.com>
2021-10-06 14:01:19 +0900

    [UPDATE] Chat api provider

--- app/src/main/java/com/example/project_flow_android/network/ApiProvider.kt
+++ app/src/main/java/com/example/project_flow_android/network/ApiProvider.kt
@@ -1,5 +1,6 @@
 package com.example.project_flow_android.network
 
+import com.example.project_flow_android.data.remote.ChatApi
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -9,9 +10,11 @@ import java.util.concurrent.TimeUnit
 
 object ApiProvider {
     private const val BASE_URL: String = "http://3.36.224.130:8080"
+    private const val BASE_URL_CHAT = "http://54.180.224.67:8080"
     private const val CONNECT_TIME_OUT: Long = 15
     private const val WRITE_TIME_OUT: Long = 15
     private const val READ_TIME_OUT: Long = 15
+    private var chatRetrofitBuilder : Retrofit
 
     private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -31,4 +34,15 @@ object ApiProvider {
         addConverterFactory(GsonConverterFactory.create())
     }.build()
 
+    init {
+        chatRetrofitBuilder = Retrofit.Builder()
+            .baseUrl(BASE_URL_CHAT)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+        chatRetrofitBuilder.create(ChatApi::class.java)
+    }
+
+    fun getChatAPI() = chatRetrofitBuilder
 }
