29a61da83c66ee55c1502225010a1b9c1d764430
Anbyeonghun <abh0920one@gmail.com>
2021-10-08 08:34:35 +0900

    [UPDATE] RecyclerView item click event

--- app/src/main/java/com/example/project_flow_android/ui/chat/CreateRVAdapter.kt
+++ app/src/main/java/com/example/project_flow_android/ui/chat/CreateRVAdapter.kt
@@ -9,6 +9,16 @@ import com.example.project_flow_android.data.model.sign.chat.ProjectMemberRespon
 import kotlinx.android.synthetic.main.chat_create_user_item.view.*
 
 class CreateRVAdapter(private val items : ProjectMemberResponse) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    interface OnItemClickListener{
+        fun onItemClick(v: View, position: Int)
+    }
+    private var itemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(itemClickListener: OnItemClickListener){
+        this.itemClickListener = itemClickListener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflateView = LayoutInflater.from(parent.context).inflate(R.layout.chat_create_user_item, parent, false)
         return ViewHolder(inflateView)
@@ -24,10 +34,17 @@ class CreateRVAdapter(private val items : ProjectMemberResponse) : RecyclerView.
 
     override fun getItemCount() = items.responses.size
 
-    inner class ViewHolder(v : View) : RecyclerView.ViewHolder(v){
+    inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
         val view = v
         fun bind(item: ProjectMemberResponse.User){
             view.create_user_item_name_tv.text = item.name
+
+            val position = absoluteAdapterPosition
+            if(position != RecyclerView.NO_POSITION){
+                itemView.setOnClickListener{
+                    itemClickListener?.onItemClick(itemView, position)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ChatCreateFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ChatCreateFragment.kt
@@ -9,11 +9,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project_flow_android.R
 import com.example.project_flow_android.ui.chat.CreateRVAdapter
 import com.example.project_flow_android.viewmodel.chat.ChatViewModel
+import kotlinx.android.synthetic.main.chat_create_user_item.view.*
 import kotlinx.android.synthetic.main.fragment_chat_create.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChatCreateFragment : Fragment() {
     private val chatViewModel : ChatViewModel by viewModel()
+    private val userState = HashMap<Int, Boolean>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,6 +34,12 @@ class ChatCreateFragment : Fragment() {
         chatViewModel.chatLiveData.observe(viewLifecycleOwner, {
             val adapter = CreateRVAdapter(chatViewModel.chatLiveData.value!!)
             chat_create_user_rv.adapter = adapter
+
+            adapter.setOnItemClickListener(object : CreateRVAdapter.OnItemClickListener{
+                override fun onItemClick(v: View, position: Int) {
+                    itemClick(v, position)
+                }
+            })
         })
 
         chat_create_btn.setOnClickListener{
@@ -41,5 +49,27 @@ class ChatCreateFragment : Fragment() {
 
     private fun getProjectUser(){
         chatViewModel.getProjectUser()
+    }
+
+    private fun itemClick(v: View, position: Int){
+        if(userState[position] == null){
+            itemSelected(v, position)
+        }
+        else{
+            if(userState[position]!!) itemDeselected(v, position)
+            else itemSelected(v, position)
+        }
+    }
+
+    private fun itemSelected(v: View, position: Int){
+        userState[position] = true
+        v.create_user_cv.setBackgroundColor(resources.getColor(R.color.color_flow, null))
+        v.create_user_item_name_tv.setTextColor(resources.getColor(R.color.white, null))
+    }
+
+    private fun itemDeselected(v: View, position: Int){
+        userState[position] = false
+        v.create_user_cv.setBackgroundColor(resources.getColor(R.color.white, null))
+        v.create_user_item_name_tv.setTextColor(resources.getColor(R.color.black, null))
     }
 }
