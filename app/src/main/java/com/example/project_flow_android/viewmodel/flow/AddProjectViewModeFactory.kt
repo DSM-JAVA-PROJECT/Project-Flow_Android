4f444a1189f72ca7a3c2c8e6e68718c57e4132f7
dlswer23 <lbj4298@naver.com>
2021-10-10 01:48:00 +0900

    [UPDATE] flow viewmodel

--- app/src/main/java/com/example/project_flow_android/viewmodel/flow/AddProjectViewModeFactory.kt
+++ app/src/main/java/com/example/project_flow_android/viewmodel/flow/AddProjectViewModeFactory.kt
@@ -1,4 +1,11 @@
 package com.example.project_flow_android.viewmodel.flow
 
-class AddProjectViewModeFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.project_flow_android.data.remote.FlowApilmpl
+
+class AddProjectViewModeFactory(private val flowApiImpl: FlowApilmpl) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        modelClass.getConstructor(flowApiImpl::class.java)
+            .newInstance(flowApiImpl)
 }
