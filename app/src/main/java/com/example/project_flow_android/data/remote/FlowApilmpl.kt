465327ab3cbe7690732f1a9da39c8f5e26454565
dlswer23 <lbj4298@naver.com>
2021-10-10 00:51:19 +0900

    [UPDATE] flow api

--- app/src/main/java/com/example/project_flow_android/data/remote/FlowApilmpl.kt
+++ app/src/main/java/com/example/project_flow_android/data/remote/FlowApilmpl.kt
@@ -1,4 +1,20 @@
 package com.example.project_flow_android.data.remote
 
+import com.example.project_flow_android.feature.AddProjectRequest
+import com.example.project_flow_android.network.ApiProvider
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
 class FlowApilmpl {
+
+    private fun providerFlowApi(): FlowApi = ApiProvider.RetroFitBuilder.create(FlowApi::class.java)
+
+    fun addProjectApi(request: AddProjectRequest):@NonNull Single<Response<Void>> = providerFlowApi().addProject(request)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+
+
 }
