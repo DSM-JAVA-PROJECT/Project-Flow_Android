66ca07a872b3aff3f4469b54a8b5a6a81efc1770
dlswer23 <lbj4298@naver.com>
2021-10-11 15:57:42 +0900

    [UPDATE] add shared preference
    user 이름을 서버에서 받아올지, shared preference로 받아올지 고민이에요

--- app/src/main/java/com/example/project_flow_android/viewmodel/flow/ProjectViewModel.kt
+++ app/src/main/java/com/example/project_flow_android/viewmodel/flow/ProjectViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.project_flow_android.viewmodel.flow
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.project_flow_android.data.SharedPreferenceStorage
 import com.example.project_flow_android.data.remote.FlowApilmpl
 
-class ProjectViewModel(private val flowApilmpl: FlowApilmpl) : ViewModel() {
+class ProjectViewModel(private val flowApilmpl: FlowApilmpl, private val sharedPreferenceStorage: SharedPreferenceStorage) : ViewModel() {
+
+    val user = MutableLiveData<String>()
+    val userName = sharedPreferenceStorage.getInfo("userName")
+
+    fun inputUser(){
+        user.value = userName.toString()
+    }
 
 
 }
