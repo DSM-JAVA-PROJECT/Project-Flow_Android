d48357d181dc7b6af90eed35c63d92a80915e074
dlswer23 <lbj4298@naver.com>
2021-10-12 19:28:45 +0900

    [UPDATE] viewmodel get()

--- app/src/main/java/com/example/project_flow_android/di/module/AboutProjectModule.kt
+++ app/src/main/java/com/example/project_flow_android/di/module/AboutProjectModule.kt
@@ -7,5 +7,5 @@ import org.koin.dsl.module
 
 val aboutProjectmodule = module {
     single { FlowApilmpl() }
-    single { AddProjectViewModeFactory(get()) }
+    single { AddProjectViewModeFactory(get(),get()) }
 }
