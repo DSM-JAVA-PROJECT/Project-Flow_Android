ddb850e60f1b80bfd14c565a11a99e5b7c098f12
Anbyeonghun <abh0920one@gmail.com>
2021-10-14 11:51:49 +0900

    [UPDATE] Chat room list api

--- app/src/main/java/com/example/project_flow_android/data/remote/chat/ChatRepository.kt
+++ app/src/main/java/com/example/project_flow_android/data/remote/chat/ChatRepository.kt
@@ -1,8 +1,10 @@
 package com.example.project_flow_android.data.remote.chat
 
 import com.example.project_flow_android.data.model.sign.chat.ProjectMemberResponse
+import com.example.project_flow_android.data.model.sign.chat.RoomListResponse
 import retrofit2.Response
 
 interface ChatRepository{
     suspend fun getProjectUser(header:String, projectId: String) : Response<ProjectMemberResponse>
+    suspend fun getRoomList(header: String, projectId: String) : Response<RoomListResponse>
 }
--- app/src/main/java/com/example/project_flow_android/network/ChatApi.kt
+++ app/src/main/java/com/example/project_flow_android/network/ChatApi.kt
@@ -1,6 +1,7 @@
 package com.example.project_flow_android.network
 
 import com.example.project_flow_android.data.model.sign.chat.ProjectMemberResponse
+import com.example.project_flow_android.data.model.sign.chat.RoomListResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -11,4 +12,9 @@ interface ChatApi {
     suspend fun getProjectUser(
         @Header("Authorization") header : String,
         @Path("projectId") projectId : String) : Response<ProjectMemberResponse>
+
+    @GET("/{projectId}")
+    suspend fun getRoomList(
+        @Header("Authorization") header: String,
+        @Path("projectId") projectId: String) : Response<RoomListResponse>
 }
