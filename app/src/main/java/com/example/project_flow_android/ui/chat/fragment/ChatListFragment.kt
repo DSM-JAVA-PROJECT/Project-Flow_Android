e7977023c03e4ff8b3a8d3656d08698628999ce9
Anbyeonghun <abh0920one@gmail.com>
2021-10-15 14:49:24 +0900

    [UPDATE] chat room item bind data

--- app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ChatListFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ChatListFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project_flow_android.R
 import com.example.project_flow_android.ui.chat.ChatActivity
+import com.example.project_flow_android.ui.chat.RoomRVAdapter
 import com.example.project_flow_android.viewmodel.chat.ChatViewModel
 import kotlinx.android.synthetic.main.fragment_chat_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChatListFragment : Fragment() {
+    private val chatViewModel : ChatViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +26,15 @@ class ChatListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        chatViewModel.getRoomList()
+        val layoutManager = LinearLayoutManager(requireContext())
+        chat_list_rv.layoutManager = layoutManager
+
+        chatViewModel.chatRoomLiveData.observe(viewLifecycleOwner, {
+            val adapter = RoomRVAdapter(chatViewModel.chatRoomLiveData.value!!)
+            chat_list_rv.adapter = adapter
+        })
 
         chat_list_add_iv.setOnClickListener{
             (activity as ChatActivity).replace(ChatCreateFragment())
