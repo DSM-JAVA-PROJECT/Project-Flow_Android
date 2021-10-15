31dc14b5fe31bbcdcebe59338d6c44e0edb99203
dlswer23 <lbj4298@naver.com>
2021-10-16 01:00:25 +0900

    [REFACTOR] module constructor change

--- app/src/main/java/com/example/project_flow_android/di/module/CertificationModule.kt
+++ app/src/main/java/com/example/project_flow_android/di/module/CertificationModule.kt
@@ -1,10 +1,12 @@
 package com.example.project_flow_android.di.module
 
 import com.example.project_flow_android.data.remote.CertificationApiImpl
+import com.example.project_flow_android.data.remote.SignApiImpl
 import com.example.project_flow_android.viewmodel.certification.CertificationViewModelFactory
 import org.koin.dsl.module
 
 val certificationModule = module {
     single { CertificationApiImpl() }
-    single { CertificationViewModelFactory(get(),get()) }
+    single { SignApiImpl() }
+    single { CertificationViewModelFactory(get(),get(),get()) }
 }
