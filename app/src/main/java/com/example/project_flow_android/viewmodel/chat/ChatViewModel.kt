4f130a33bd3911fbbe2742334c94b93acb1a1d37
Anbyeonghun <abh0920one@gmail.com>
2021-10-21 11:34:44 +0900

    [UPDATE] Socket.io connection

--- app/src/main/java/com/example/project_flow_android/viewmodel/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/project_flow_android/viewmodel/chat/ChatViewModel.kt
@@ -8,17 +8,18 @@ import androidx.lifecycle.viewModelScope
 import com.example.project_flow_android.data.model.sign.chat.ProjectMemberResponse
 import com.example.project_flow_android.data.model.sign.chat.RoomListResponse
 import com.example.project_flow_android.data.remote.chat.ChatRepositoryImpl
+import io.socket.client.IO
+import io.socket.client.Socket
 import kotlinx.coroutines.launch
 import org.json.JSONObject
 import ua.naiksoftware.stomp.Stomp
-import ua.naiksoftware.stomp.dto.LifecycleEvent
-import ua.naiksoftware.stomp.dto.StompHeader
 
 class ChatViewModel : ViewModel() {
     private val TAG = "StompClient"
-    private val URL = "ws://54.180.224.67:8080/websocket"
+    private val URL = "http://ca7c-223-39-206-60.ngrok.io"
     private val access_token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzZXJ2ZXIiLCJpYXQiOjE2MzQyODMzMjMsImlkIjoiNjE2N2JhNTQyNjdjYTEwZWI1NDkwNGE5IiwiZW1haWwiOiJhYmgwOTIwb25lQGdtYWlsLmNvbSJ9.Y_smWBnm1RrvToFW9kB9pHhnmgZIu0O73OZH4Cy3iZ4"
     private val stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, URL)
+    private lateinit var socket : Socket
     private var chatRoomId = 0
     private var projectId = "616c247724ffea704e117e6e"
 
@@ -30,18 +31,29 @@ class ChatViewModel : ViewModel() {
 
     fun connect(){
         Thread {
-            val headerList = ArrayList<StompHeader>()
-            headerList.add(StompHeader("Authorization", access_token))
-
-            stompClient.lifecycle().subscribe{
-                when(it.type){
-                    LifecycleEvent.Type.OPENED -> Log.d(TAG, "stomp connection opened")
-                    LifecycleEvent.Type.ERROR -> Log.d(TAG, "stomp connection error")
-                    LifecycleEvent.Type.CLOSED -> Log.d(TAG, "stomp connection closed")
-                    LifecycleEvent.Type.FAILED_SERVER_HEARTBEAT -> Log.d(TAG, "stomp failed server heartbeat")
-                }
+            try {
+                socket = IO.socket(URL)
+            } catch (e: Exception){
+                e.printStackTrace()
             }
-            stompClient.connect(headerList)
+//            val headerList = ArrayList<StompHeader>()
+//            headerList.add(StompHeader("Authorization", access_token))
+//
+//            stompClient.lifecycle().subscribe{
+//                when(it.type){
+//                    LifecycleEvent.Type.OPENED -> Log.d(TAG, "stomp connection opened")
+//                    LifecycleEvent.Type.ERROR -> Log.d(TAG, "stomp connection error")
+//                    LifecycleEvent.Type.CLOSED -> Log.d(TAG, "stomp connection closed")
+//                    LifecycleEvent.Type.FAILED_SERVER_HEARTBEAT -> Log.d(TAG, "stomp failed server heartbeat")
+//                }
+//            }
+//            stompClient.connect(headerList)
+            socket.connect()
+            socket.on(Socket.EVENT_CONNECT) {
+                Log.i("Socket", "Connect")
+            }.on(Socket.EVENT_DISCONNECT) { args ->
+                Log.i("Socket", "Disconnet: ${args[0]}")
+            }
         }.start()
     }
 
