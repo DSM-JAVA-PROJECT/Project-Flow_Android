7a0dc3a58917a5e7ee45952acfa65ad136bf2e10
dlswer23 <lbj4298@naver.com>
2021-10-29 23:20:07 +0900

    [UPDATE] add change password viewmodel

--- app/src/main/java/com/example/project_flow_android/di/module/MyPageModule.kt
+++ app/src/main/java/com/example/project_flow_android/di/module/MyPageModule.kt
@@ -1,9 +1,12 @@
 package com.example.project_flow_android.di.module
 
+import com.example.project_flow_android.viewmodel.mypage.ChangePasswordViewModel
 import com.example.project_flow_android.viewmodel.mypage.MyPageViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val mypageModule = module {
     viewModel { MyPageViewModel(get(),get()) }
+
+    viewModel { ChangePasswordViewModel()}
 }
