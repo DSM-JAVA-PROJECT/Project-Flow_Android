a56ad6a0c0916f4746c1bbf18d4bd8b3b3ccffb6
dlswer23 <lbj4298@naver.com>
2021-11-10 17:16:18 +0900

    Revert "Merge branch 'develop' into feature/mypage"
    This reverts commit 7e2346a6df6feabf7b1bae572d6c97aa159b5d1f.

--- app/src/main/java/com/example/project_flow_android/feature/ImageRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.example.project_flow_android.feature
-
-import java.io.File
-
-data class ImageRequest(val projectIamge : File)
--- app/src/main/java/com/example/project_flow_android/ui/flow/FlowFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/flow/FlowFragment.kt
@@ -1,8 +1,5 @@
 package com.example.project_flow_android.ui.flow
 
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.FragmentTransaction
 import com.example.project_flow_android.R
 import com.example.project_flow_android.base.BaseFragment
 import com.example.project_flow_android.databinding.FragmentFlowBinding
@@ -10,38 +7,12 @@ import com.example.project_flow_android.viewmodel.flow.FlowViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class FlowFragment : BaseFragment<FragmentFlowBinding>(R.layout.fragment_flow) {
+class FlowFragment :BaseFragment<FragmentFlowBinding>(R.layout.fragment_flow){
+
 
     override val vm: FlowViewModel by viewModel()
 
-    val secondfragment = AddProjectFragment()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        observeEvent()
-
-    }
-
-
-    fun changeFragment() {
-        childFragmentManager.beginTransaction()
-            .replace(R.id.fragment, secondfragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .commit()
-    }
-
-    fun addProject() {
-        binding.addprojectBtn.setOnClickListener {
-            changeFragment()
-        }
-    }
-
     override fun observeEvent() {
-        vm.run {
-            getUserInfo()
-            addProject()
-        }
     }
 
 }
