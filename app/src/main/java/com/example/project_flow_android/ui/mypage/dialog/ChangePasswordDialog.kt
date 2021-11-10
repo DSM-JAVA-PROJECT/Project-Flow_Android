39bd8213f099f91522141b0a918052c61dcb2b85
dlswer23 <lbj4298@naver.com>
2021-11-10 15:47:22 +0900

    Revert "Merge branch 'develop' into feature/flow"
    This reverts commit a3c9d524a653843501b795fb205ddea3d6ff4c5f.

--- app/src/main/java/com/example/project_flow_android/ui/mypage/dialog/ChangePasswordDialog.kt
+++ app/src/main/java/com/example/project_flow_android/ui/mypage/dialog/ChangePasswordDialog.kt
@@ -3,7 +3,6 @@ package com.example.project_flow_android.ui.mypage.dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.lifecycle.LifecycleOwner
 import com.example.project_flow_android.R
 import com.example.project_flow_android.base.BaseDialog
 import com.example.project_flow_android.databinding.DialogChangepasswordBinding
@@ -16,20 +15,18 @@ class ChangePasswordDialog(override val vm: ChangePasswordViewModel) :
         super.onViewCreated(view, savedInstanceState)
 
         binding.confirmTv.setOnClickListener {
-               vm.run {
-                   changePassword()
-                   successChange.observe(viewLifecycleOwner, {
-                       if(it) {
-                           Toast.makeText(context, "비밀번호 변경에 성공하였습니다", Toast.LENGTH_SHORT).show()
-                           dismiss()
-                       }
-                   })
-               }
+                vm.changePassword()
         }
 
         binding.cancleTv.setOnClickListener {
             dismiss()
         }
+
+        vm.successChange.observe(viewLifecycleOwner, {
+            if(it) {
+                dismiss()
+            }
+        })
     }
 
 }
