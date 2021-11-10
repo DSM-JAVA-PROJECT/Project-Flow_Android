ae13dc53d789ad55410c977628530fa7b91bca3c
dlswer23 <lbj4298@naver.com>
2021-11-10 17:21:41 +0900

    [FIX] mypage impl changed

--- app/src/main/java/com/example/project_flow_android/data/remote/mypage/MyPageApiImpl.kt
+++ app/src/main/java/com/example/project_flow_android/data/remote/mypage/MyPageApiImpl.kt
@@ -23,7 +23,7 @@ class MyPageApiImpl {
 
     fun changePassword(
         token: String,
-        password: String,
+        password: NewPasswordRequest,
     ): @NonNull Single<Response<Void>> =
         providerMypageApi().changePassword(token, password)
             .observeOn(AndroidSchedulers.mainThread())
