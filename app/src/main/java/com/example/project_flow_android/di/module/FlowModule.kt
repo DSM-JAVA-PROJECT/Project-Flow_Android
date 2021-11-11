d5bf389cd7a19ef62328d7e2ff3f8925fd469cb9
dlswer23 <lbj4298@naver.com>
2021-11-11 18:57:27 +0900

    [UPDATE] flow module
    di 추가

--- app/src/main/java/com/example/project_flow_android/di/module/FlowModule.kt
+++ app/src/main/java/com/example/project_flow_android/di/module/FlowModule.kt
@@ -1,10 +1,13 @@
 package com.example.project_flow_android.di.module
 
+import com.example.project_flow_android.data.remote.mypage.MyPageApiImpl
 import com.example.project_flow_android.viewmodel.flow.FlowViewModel
 import org.koin.android.compat.ScopeCompat.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val flowModule = module {
-    viewModel { FlowViewModel() }
+    viewModel { FlowViewModel(get(),get()) }
+
+    single { MyPageApiImpl() }
 }
