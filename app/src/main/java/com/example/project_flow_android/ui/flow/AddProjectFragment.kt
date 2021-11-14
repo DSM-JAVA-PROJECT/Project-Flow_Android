4dc3ec8336f03dc6139e42c2c17ee0610b0ef7a3
dlswer23 <lbj4298@naver.com>
2021-11-14 22:09:19 +0900

    [UPDATE] add fragment

--- app/src/main/java/com/example/project_flow_android/ui/flow/AddProjectFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/flow/AddProjectFragment.kt
@@ -6,30 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.project_flow_android.R
+import com.example.project_flow_android.base.BaseFragment
+import com.example.project_flow_android.databinding.FragmentAddProjectBindingImpl
+import com.example.project_flow_android.ui.main.MainActivity
+import com.example.project_flow_android.viewmodel.flow.AddProjectViewModel
+import com.example.project_flow_android.viewmodel.flow.FlowViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class AddProjectFragment : Fragment() {
+class AddProjectFragment :
+    BaseFragment<FragmentAddProjectBindingImpl>(R.layout.fragment_add_project) {
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-
-        goFlowPage()
-
-        return inflater.inflate(R.layout.fragment_add_project, container, false)
-    }
+    override val vm: AddProjectViewModel by viewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
 
-    private fun goFlowPage() {
 
     }
 
-     fun observeEvent() {
+    override fun observeEvent() {
         TODO("Not yet implemented")
     }
 
--- app/src/main/java/com/example/project_flow_android/ui/flow/FlowFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/flow/FlowFragment.kt
@@ -1,17 +1,37 @@
 package com.example.project_flow_android.ui.flow
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.LifecycleOwner
 import com.example.project_flow_android.R
 import com.example.project_flow_android.base.BaseFragment
 import com.example.project_flow_android.databinding.FragmentFlowBinding
+import com.example.project_flow_android.ui.main.MainActivity
 import com.example.project_flow_android.viewmodel.flow.FlowViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class FlowFragment :BaseFragment<FragmentFlowBinding>(R.layout.fragment_flow){
 
-
     override val vm: FlowViewModel by viewModel()
 
+    private val addProject =  AddProjectFragment()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vm.run {
+            getMainUserInfo()
+            emptyProject.observe(viewLifecycleOwner, {
+                binding.emptyProjectImg.visibility
+                binding.emptyProjectTv.visibility
+            })
+            binding.addProjectBtn.setOnClickListener{
+                //(activity as MainActivity).addProject()
+                (activity as MainActivity).changeFragment(addProject)
+            }
+        }
+
+    }
     override fun observeEvent() {
     }
 
