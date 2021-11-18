9a7c08c1dda1c181a4e5715f9ab51eaa23e09132
dlswer23 <lbj4298@naver.com>
2021-11-19 00:46:40 +0900

    [UPDATE] oauth webview

--- app/src/main/java/com/example/project_flow_android/ui/sign/ui/LoginActivity.kt
+++ app/src/main/java/com/example/project_flow_android/ui/sign/ui/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.project_flow_android.ui.sign.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.lifecycle.ViewModelProvider
 import com.example.project_flow_android.R
 import com.example.project_flow_android.base.BaseActivity
@@ -9,6 +11,7 @@ import com.example.project_flow_android.databinding.ActivityLoginBinding
 import com.example.project_flow_android.ui.main.MainActivity
 import com.example.project_flow_android.viewmodel.login.LoginViewModel
 import com.example.project_flow_android.viewmodel.login.LoginViewModelFactory
+import kotlinx.android.synthetic.main.activity_login.*
 import org.koin.android.ext.android.inject
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
@@ -21,19 +24,33 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         successfulLogin()
+
+        binding.oauthBtn.setOnClickListener {
+            web_view.apply {
+                getOauth()
+            }
+        }
+
+    }
+
+    private fun getOauth() {
+        binding.webView.visibility
+        val webView = findViewById<WebView>(R.id.web_view)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://18.210.203.222:8080/auth/oauth")
+
     }
 
     private fun successfulLogin() {
         vm.run {
-           successLogin.observe(this@LoginActivity,{
-               if(it) {
-                   val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                   startActivity(intent)
-                   binding.comment2Tv.text = vm.changeComment.value
-               }
-               binding.comment2Tv.text = vm.changeComment.value
-
-           })
+            successLogin.observe(this@LoginActivity, {
+                if (it) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    binding.comment2Tv.text = vm.changeComment.value
+                }
+                binding.comment2Tv.text = vm.changeComment.value
+            })
 
         }
     }
