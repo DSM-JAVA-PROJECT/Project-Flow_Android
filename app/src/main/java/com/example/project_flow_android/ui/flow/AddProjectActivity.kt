4afff75f3c1e7cbafbe7434183c0b9e1c370553e
dlswer23 <lbj4298@naver.com>
2021-11-27 02:08:40 +0900

    [UPDATE] remove check empty project

--- app/src/main/java/com/example/project_flow_android/ui/flow/AddProjectActivity.kt
+++ app/src/main/java/com/example/project_flow_android/ui/flow/AddProjectActivity.kt
@@ -2,14 +2,24 @@ package com.example.project_flow_android.ui.flow
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import com.example.project_flow_android.R
 import com.example.project_flow_android.base.BaseActivity
 import com.example.project_flow_android.data.remote.toRealPath
 import com.example.project_flow_android.databinding.ActivityAddProjectBinding
+import com.example.project_flow_android.ui.main.MainActivity
 import com.example.project_flow_android.viewmodel.flow.AddProjectViewModel
 import gun0912.tedimagepicker.builder.TedRxImagePicker
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.InputStream
 
 class AddProjectActivity : BaseActivity<ActivityAddProjectBinding>(R.layout.activity_add_project) {
 
@@ -34,8 +44,8 @@ class AddProjectActivity : BaseActivity<ActivityAddProjectBinding>(R.layout.acti
     fun observeEvent() {
         vm.run {
             successAddProject.observe(this@AddProjectActivity, {
-                val intent = Intent(this@AddProjectActivity, FlowFragment::class.java)
-                startActivity(intent)
+//                val intent = Intent(this@AddProjectActivity, MainActivity::class.java)
+//                startActivity(intent)
             })
             binding.getImg.setOnClickListener {
                 getImage()
--- app/src/main/java/com/example/project_flow_android/ui/flow/FlowFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/flow/FlowFragment.kt
@@ -1,17 +1,13 @@
 package com.example.project_flow_android.ui.flow
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.LifecycleOwner
+import androidx.core.view.isVisible
 import com.example.project_flow_android.R
 import com.example.project_flow_android.base.BaseFragment
-import com.example.project_flow_android.data.SharedPreferenceStorage
 import com.example.project_flow_android.databinding.FragmentFlowBinding
-import com.example.project_flow_android.databinding.ItemFlowViewBinding
 import com.example.project_flow_android.ui.flow.dialog.FinishProjectDialog
 import com.example.project_flow_android.ui.main.MainActivity
-import com.example.project_flow_android.ui.mypage.dialog.ChangePasswordDialog
 import com.example.project_flow_android.viewmodel.flow.FlowViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -44,33 +40,21 @@ class FlowFragment() :BaseFragment<FragmentFlowBinding>(R.layout.fragment_flow){
         viewPager.adapter = adapter
         TabLayoutMediator(dotsIndicator, viewPager) {_, _ ->
         }.attach()
-
-        rvSet(1)
-
         observeEvent()
         binding.addProjectBtn.setOnClickListener{
             (activity as MainActivity).addProject()
         }
     }
 
-    private fun rvSet(position : Int){
-        vm.run {
-            getMainInfo.observe(viewLifecycleOwner,{
-                mainFlowViewPagerRVAdapter.setItem(it.getProject)
-            })
-        }
-
-    }
-
     override fun observeEvent() {
         vm.run {
             getMainUserInfo()
-            emptyProject.observe(viewLifecycleOwner, {
-                binding.emptyProjectImg.visibility
-                binding.emptyProjectTv.visibility
-            })
             getProjectDetailInfo()
-
+            vm.run {
+                getMainInfo.observe(viewLifecycleOwner,{
+                    mainFlowViewPagerRVAdapter.setItem(it.getProject)
+                })
+            }
 
         }
     }
