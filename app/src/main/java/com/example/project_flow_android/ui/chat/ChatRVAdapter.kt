49484946feb41ddd466a7e08c016562baf7cba41
Anbyeonghun <abh0920one@gmail.com>
2021-11-28 18:18:54 +0900

    [MODIFY] image bind function

--- app/src/main/java/com/example/project_flow_android/ui/chat/ChatRVAdapter.kt
+++ app/src/main/java/com/example/project_flow_android/ui/chat/ChatRVAdapter.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -39,16 +40,19 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
     private val INVITE = 8
     private val LEAVE = 9
 
-    interface OnJoinClickListener{
+    interface OnJoinClickListener {
         fun onJoinClick(v: View, position: Int)
     }
-    interface OnReJoinClickListener{
+
+    interface OnReJoinClickListener {
         fun onReJoinClick(v: View, position: Int)
     }
-    interface OnShowClickListener{
+
+    interface OnShowClickListener {
         fun onShowClick(v: View, position: Int)
     }
-    interface OnResignClickListener{
+
+    interface OnResignClickListener {
         fun onResignClick(v: View, position: Int)
     }
 
@@ -57,19 +61,19 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
     private var showClickListener: OnShowClickListener? = null
     private var resignClickListener: OnResignClickListener? = null
 
-    fun setOnJoinClickListener(joinClickListener : OnJoinClickListener){
+    fun setOnJoinClickListener(joinClickListener: OnJoinClickListener) {
         this.joinClickListener = joinClickListener
     }
 
-    fun setOnReJoinClickListener(reJoinClickListener : OnReJoinClickListener){
+    fun setOnReJoinClickListener(reJoinClickListener: OnReJoinClickListener) {
         this.reJoinClickListener = reJoinClickListener
     }
 
-    fun setOnShowClickListener(showClickListener: OnShowClickListener){
+    fun setOnShowClickListener(showClickListener: OnShowClickListener) {
         this.showClickListener = showClickListener
     }
 
-    fun setOnResignClickListener(resignClickListener: OnResignClickListener){
+    fun setOnResignClickListener(resignClickListener: OnResignClickListener) {
         this.resignClickListener = resignClickListener
     }
 
@@ -121,7 +125,7 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = items.oldChatMessageResponses[position]
-        when(holder){
+        when (holder) {
             is MineViewHolder -> holder.apply { bind(item) }
             is OtherViewHolder -> holder.apply { bind(item) }
             is AddPlanViewHolder -> holder.apply { bind(item) }
@@ -139,25 +143,25 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
 
     override fun getItemViewType(position: Int): Int {
         val item = items.oldChatMessageResponses[position]
-        return when(item.mine){
+        return when (item.mine) {
             true -> {
-                when(item.type){
+                when (item.type) {
                     "MESSAGE" -> MINE_TALK
                     "PLAN" -> PLAN_ADD
                     "JOIN_PLAN" -> MINE_JOIN_PLAN
                     "RESIGN_PLAN" -> MINE_RESIGN_PLAN
                     "FORCED_PLAN" -> FORCED_PLAN
                     "CHATROOM" -> {
-                        if(item.profileImage == null){
+                        if (item.profileImage == null) {
                             INVITE
-                        }else
+                        } else
                             LEAVE
                     }
                     else -> throw RuntimeException("알 수 없는 타입")
                 }
             }
             false -> {
-                when(item.type){
+                when (item.type) {
                     "MESSAGE" -> OTHER_TALK
                     "PLAN" -> PLAN_ADD
                     "JOIN_PLAN" -> OTHER_JOIN_PLAN
@@ -183,15 +187,12 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
             view.chat_other_name_tv.text = item.senderName
             view.chat_other_content_tv.text = item.message
             view.chat_other_cnt_tv.text = item.readerList.size.toString()
-            if(item.senderImage != null){
-                view.chat_other_profile_iv.clipToOutline = true
-                Glide.with(activity).load(Uri.parse(item.senderImage)).into(view.chat_other_profile_iv)
-            }
+            imageLoad(view.chat_other_profile_iv, item.senderImage)
             dateFormat(item.createdAt, view.chat_other_time_tv)
         }
     }
 
-    inner class AddPlanViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class AddPlanViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
         fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.plan_item_mine_content_tv.text = item.planName
@@ -199,55 +200,52 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
             dateFormat(item.createdAt, view.plan_item_mine_time_tv)
 
             val position = absoluteAdapterPosition
-            if(position != RecyclerView.NO_POSITION){
-                itemView.plan_item_mine_join_btn.setOnClickListener{
+            if (position != RecyclerView.NO_POSITION) {
+                itemView.plan_item_mine_join_btn.setOnClickListener {
                     joinClickListener?.onJoinClick(itemView, position)
                 }
             }
         }
     }
 
-    inner class JoinPlanMineViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class JoinPlanMineViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
-        fun bind(item: ChatMessageResponse.ChatReceiveResponse){
+        fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.join_plan_item_mine_content_tv.text = item.planName
             view.join_plan_item_mine_date_tv.text = "${item.startDate} ~ ${item.endDate}"
             dateFormat(item.createdAt, view.join_plan_item_mine_time_tv)
 
             val position = absoluteAdapterPosition
-            if(position != RecyclerView.NO_POSITION){
-                itemView.join_plan_item_mine_join_btn.setOnClickListener{
+            if (position != RecyclerView.NO_POSITION) {
+                itemView.join_plan_item_mine_view_btn.setOnClickListener {
                     showClickListener?.onShowClick(itemView, position)
                 }
-                itemView.join_plan_item_mine_exit_btn.setOnClickListener{
+                itemView.join_plan_item_mine_exit_btn.setOnClickListener {
                     resignClickListener?.onResignClick(itemView, position)
                 }
             }
         }
     }
 
-    inner class JoinPlanOtherViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class JoinPlanOtherViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
         fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.join_plan_item_other_content_tv.text = item.planName
             view.join_plan_item_other_date_tv.text = "${item.startDate} ~ ${item.endDate}"
             view.join_plan_item_other_cnt_tv.text = item.readerList.size.toString()
             dateFormat(item.createdAt, view.join_plan_item_other_time_tv)
-            if(item.senderImage != null){
-                view.join_chat_other_profile_iv.clipToOutline = true
-                Glide.with(activity).load(Uri.parse(item.senderImage)).into(view.join_chat_other_profile_iv)
-            }
+            imageLoad(view.join_chat_other_profile_iv, item.senderImage)
 
             val position = absoluteAdapterPosition
-            if(position != RecyclerView.NO_POSITION){
-                itemView.join_plan_item_other_btn.setOnClickListener{
+            if (position != RecyclerView.NO_POSITION) {
+                itemView.join_plan_item_other_btn.setOnClickListener {
                     showClickListener?.onShowClick(itemView, position)
                 }
             }
         }
     }
 
-    inner class ResignPlanMineViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class ResignPlanMineViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
         fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.resign_plan_item_mine_content_tv.text = item.planName
@@ -256,35 +254,32 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
             dateFormat(item.createdAt, view.resign_plan_item_mine_time_tv)
 
             val position = absoluteAdapterPosition
-            if(position != RecyclerView.NO_POSITION){
-                itemView.resign_plan_item_mine_join_btn.setOnClickListener{
+            if (position != RecyclerView.NO_POSITION) {
+                itemView.resign_plan_item_mine_join_btn.setOnClickListener {
                     reJoinClickListener?.onReJoinClick(itemView, position)
                 }
             }
         }
     }
 
-    inner class ResignPlanOtherViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class ResignPlanOtherViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
         fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.resign_plan_item_other_content_tv.text = item.planName
             view.resign_plan_item_other_date_tv.text = "${item.startDate} ~ ${item.endDate}"
             view.resign_plan_item_other_cnt_tv.text = item.readerList.size.toString()
             dateFormat(item.createdAt, view.resign_plan_item_other_time_tv)
-            if(item.senderImage != null){
-                view.resign_chat_other_profile_iv.clipToOutline = true
-                Glide.with(activity).load(Uri.parse(item.senderImage)).into(view.resign_chat_other_profile_iv)
-            }
+            imageLoad(view.resign_chat_other_profile_iv, item.senderImage)
 
             val position = absoluteAdapterPosition
-            if(position != RecyclerView.NO_POSITION){
-                itemView.resign_plan_item_other_btn.setOnClickListener{
+            if (position != RecyclerView.NO_POSITION) {
+                itemView.resign_plan_item_other_btn.setOnClickListener {
                 }
             }
         }
     }
 
-    inner class ForcedPlanViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class ForcedPlanViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
         fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.forced_plan_item_mine_content_tv.text = item.planName
@@ -293,14 +288,14 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
         }
     }
 
-    inner class InviteRoomViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class InviteRoomViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
         fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.room_status_tv.text = "${item.userName}님이 채팅방에 입장했습니다."
         }
     }
 
-    inner class LeaveRoomViewHolder(v: View) : RecyclerView.ViewHolder(v){
+    inner class LeaveRoomViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val view = v
         fun bind(item: ChatMessageResponse.ChatReceiveResponse) {
             view.room_status_tv.text = "${item.userName}님이 나가셨습니다."
@@ -321,18 +316,23 @@ class ChatRVAdapter(private val items: ChatMessageResponse, private val activity
         }
     }
 
-    private fun dateFormat(date: String, tv: TextView){
+    private fun dateFormat(date: String, tv: TextView) {
         val old_format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
         val new_format = SimpleDateFormat("HH:mm")
-
         try {
             val old_date = old_format.parse(date)
             val new_date = new_format.format(old_date)
             tv.text = new_date
-        } catch (e: ParseException){
+        } catch (e: ParseException) {
             e.printStackTrace()
         }
     }
 
-   // private fun readCount(readerList : ArrayList<String>) = readerList.size
+    private fun imageLoad(view: ImageView, image: String?){
+        if (image != null) {
+            view.clipToOutline = true
+            Glide.with(activity).load(Uri.parse(image))
+                .into(view)
+        }
+    }
 }
