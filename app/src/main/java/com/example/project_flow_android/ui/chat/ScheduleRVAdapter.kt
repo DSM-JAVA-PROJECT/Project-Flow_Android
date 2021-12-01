3c352bd8744bb24c0dfd8e01c751825bb3392fbe
Anbyeonghun <abh0920one@gmail.com>
2021-12-01 14:53:41 +0900

    [UPDATE] Schedule RV adapter

---
+++ app/src/main/java/com/example/project_flow_android/ui/chat/ScheduleRVAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.project_flow_android.ui.chat
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project_flow_android.R
+import com.example.project_flow_android.data.model.sign.chat.DatePlanResponse
+import kotlinx.android.synthetic.main.schedule_item.view.*
+
+class ScheduleRVAdapter(private val items: DatePlanResponse) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflateView = LayoutInflater.from(parent.context).inflate(R.layout.schedule_item, parent, false)
+        return ViewHolder(inflateView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is ViewHolder){
+            holder.apply {
+                bind(items.planDetails[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = items.planDetails.size
+
+    inner class ViewHolder(v : View) : RecyclerView.ViewHolder(v){
+        val view = v
+        fun bind(item: DatePlanResponse.DateResponse){
+            view.schedule_plan_tv.text = item.name
+            if(!item.isFinish){
+                view.schedule_iv.setImageResource(R.drawable.ic_baseline_circle_24_finish)
+            }
+        }
+    }
+}
