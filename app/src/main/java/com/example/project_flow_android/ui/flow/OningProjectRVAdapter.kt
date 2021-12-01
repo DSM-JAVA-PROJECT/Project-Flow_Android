992934fdf7ed70a1dcd5dc4daf5c85d634e7e383
dlswer23 <lbj4298@naver.com>
2021-12-02 01:08:37 +0900

    [UPDATE] recyclerview adapter change view holder

--- app/src/main/java/com/example/project_flow_android/ui/flow/OningProjectRVAdapter.kt
+++ app/src/main/java/com/example/project_flow_android/ui/flow/OningProjectRVAdapter.kt
@@ -7,43 +7,45 @@ import com.example.project_flow_android.databinding.UserProjectOningItemBinding
 import com.example.project_flow_android.databinding.UserProjectPlanItemBinding
 import com.example.project_flow_android.feature.GetMainInfoDetailResponse
 import com.example.project_flow_android.feature.GetMainInfoResponse
+import com.example.project_flow_android.feature.GetProjectScheduleDetailResponse
 import com.example.project_flow_android.feature.Projects
 import com.example.project_flow_android.viewmodel.flow.FlowViewModel
 
 class OningProjectRVAdapter(
-    private val viewModel: FlowViewModel
+    private val viewModel: FlowViewModel,
 ) :
     RecyclerView.Adapter<OningProjectRVAdapter.ProjectViewHolder>() {
-
-    private var projectList = ArrayList<GetMainInfoResponse.GetMainInfoDetailResponse>()
+    private var projectList = ArrayList<GetProjectScheduleDetailResponse>()
 
     inner class ProjectViewHolder(private val binding: UserProjectOningItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: GetMainInfoResponse.GetMainInfoDetailResponse, position: Int) {
-            binding.scheduleContent = item.before[position].name
+        fun bind(item: GetProjectScheduleDetailResponse, position: Int) {
+            binding.scheduleContent = item.name
             binding.scheduleStartPeriod =
-                "${item.before[position].startDate} ~ ${item.before[position].endDate}"
+                "${item.startDate} ~ ${item.endDate}"
             binding.vm = viewModel
             binding.notifyChange()
         }
     }
 
-    fun setItem(projects: ArrayList<GetMainInfoResponse.GetMainInfoDetailResponse>) {
-        this.projectList = projects
+    fun setItem(projects: List<GetProjectScheduleDetailResponse>) {
+        this.projectList = projects as ArrayList<GetProjectScheduleDetailResponse>
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectViewHolder {
-        val binding = UserProjectOningItemBinding.inflate(LayoutInflater.from(parent.context))
+        val binding =
+            UserProjectOningItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ProjectViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return projectList.size
     }
 
     override fun onBindViewHolder(holder: ProjectViewHolder, position: Int) {
         holder.bind(projectList[position], position)
     }
 
-    override fun getItemCount(): Int {
-        return projectList.size
-    }
 }
