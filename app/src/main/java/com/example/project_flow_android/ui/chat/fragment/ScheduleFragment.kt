025356b4d55c5b6c1fa76801d51014f189be70a1
Anbyeonghun <abh0920one@gmail.com>
2021-12-02 23:49:03 +0900

    [UPDATE] Schedule

--- app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ScheduleFragment.kt
+++ app/src/main/java/com/example/project_flow_android/ui/chat/fragment/ScheduleFragment.kt
@@ -1,21 +1,22 @@
 package com.example.project_flow_android.ui.chat.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.applikeysolutions.cosmocalendar.model.Day
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project_flow_android.R
 import com.example.project_flow_android.ui.chat.ChatActivity
+import com.example.project_flow_android.ui.chat.ScheduleRVAdapter
+import com.example.project_flow_android.util.HavePlanDecorator
 import com.example.project_flow_android.viewmodel.chat.ChatViewModel
-import kotlinx.android.synthetic.main.fragment_chat.*
+import com.prolificinteractive.materialcalendarview.CalendarDay
 import kotlinx.android.synthetic.main.fragment_schedule.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.net.Socket
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.Collections.singleton
 
 class ScheduleFragment : Fragment() {
 
@@ -34,19 +35,39 @@ class ScheduleFragment : Fragment() {
         val currentTime = Calendar.getInstance().time
         val yearFormat = SimpleDateFormat("yyyy", Locale.getDefault())
         val monthFormat = SimpleDateFormat("MM", Locale.getDefault())
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
         val year = yearFormat.format(currentTime)
         val month = monthFormat.format(currentTime)
 
+        val layoutManager = LinearLayoutManager(requireContext())
+        schedule_rv.layoutManager = layoutManager
+
         viewModel.getMonthPlan(year, month)
-        schedule_prev_btn.setOnClickListener{
+        schedule_prev_btn.setOnClickListener {
             (activity as ChatActivity).popBackStack(ScheduleFragment())
         }
 
         viewModel.monthPlanLiveData.observe(viewLifecycleOwner, { it ->
             it.getContentIfNotHandled()?.let {
-//                schedule_cv.
-//                schedule_cv.selectionManager.toggleDay(it.monthPlans[1].endDate)
+                val data = it.monthPlans
+                for (i in 0 until data.size) {
+                    val startDate = data[i].startDate
+                    val endDate = data[i].endDate
+
+                    schedule_cv.addDecorator(HavePlanDecorator(singleton(CalendarDay.from(dateFormat.parse(
+                        startDate))), data[i].isFinish))
+                }
             }
         })
+        viewModel.datePlanLiveData.observe(viewLifecycleOwner, { it ->
+            it.getContentIfNotHandled()?.let {
+                val adapter = ScheduleRVAdapter(it)
+                schedule_rv.adapter = adapter
+            }
+        })
+
+        schedule_cv.setOnDateChangedListener { widget, date, selected ->
+            viewModel.getDatePlan(dateFormat.format(date.date).toString())
+        }
     }
 }
