07beecdb7cc534cd9592cae645fd441572d1b4a7
Anbyeonghun <abh0920one@gmail.com>
2021-12-03 09:21:03 +0900

    [UPDATE] Calendar decorator

--- app/src/main/java/com/example/project_flow_android/util/HavePlanDecorator.kt
+++ app/src/main/java/com/example/project_flow_android/util/HavePlanDecorator.kt
@@ -6,16 +6,16 @@ import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
 
-class HavePlanDecorator(dates: Collection<CalendarDay>, finish: Boolean) : DayViewDecorator {
+class HavePlanDecorator(dates: Collection<CalendarDay>, isEndDate: Boolean) : DayViewDecorator {
     private val mDates = dates
-    private val isFinish = finish
+    private val isEnd = isEndDate
 
     override fun shouldDecorate(day: CalendarDay?): Boolean {
         return mDates.contains(day)
     }
 
     override fun decorate(view: DayViewFacade?) {
-        if(isFinish)
+        if(isEnd)
             view?.addSpan(DotSpan(5F, Color.parseColor("#FF7979")))
         else
             view?.addSpan(DotSpan(5F, Color.parseColor("#1F43C2")))
