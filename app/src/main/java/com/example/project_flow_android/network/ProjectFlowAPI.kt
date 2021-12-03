af21b1d72275d8e7b9f2971f5979f0a08a9b084c
dlswer23 <lbj4298@naver.com>
2021-12-03 10:02:15 +0900

    [UPDATE] change request type

--- app/src/main/java/com/example/project_flow_android/network/ProjectFlowAPI.kt
+++ app/src/main/java/com/example/project_flow_android/network/ProjectFlowAPI.kt
@@ -12,7 +12,7 @@ import java.io.File
 interface ProjectFlowAPI {
 
     @POST("/auth/join")
-    fun register(@Body request:RegisterRequest): Single<Response<Void>>
+    fun register(@Body request: RegisterRequest): Single<Response<Void>>
 
     @POST("/auth/login")
     fun login(@Body request: LoginRequest): Single<Response<LoginResponse>>
@@ -27,21 +27,27 @@ interface ProjectFlowAPI {
     fun getuserInfo(@Header("Authorization") token: String): Single<Response<GetUserInfoResponse>>
 
     @PATCH("/auth/password")
-    fun changePassword(@Header("Authorization") token :String, @Query("password") password : String) : Single<Response<Void>>
+    fun changePassword(
+        @Header("Authorization") token: String,
+        @Query("password") password: String,
+    ): Single<Response<Void>>
 
     @GET("/main")
-    fun getMainInfo(@Header("Authorization") token: String) : Single<Response<GetMainInfoResponse>>
+    fun getMainInfo(@Header("Authorization") token: String): Single<Response<GetMainInfoResponse>>
 
     @Multipart
-    @FormUrlEncoded
     @POST("/project")
-    fun addProject(@Header("Authorization") token: String, @Body request: AddProjectRequest, @Part("file") file: MultipartBody.Part) : Single<Response<GetProjectsId>>
+    fun addProject(
+        @Header("Authorization") token: String,
+        @Body request: AddProjectRequest,
+        @Part("file") file: MultipartBody.Part,
+    ): Single<Response<GetProjectsId>>
 
     @PATCH("/project/close/{id}")
-    fun finishProject(@Header("Authorization") token: String,@Path("id") projectId : String) : Single<Response<Void>>
-
-    @DELETE("/project/{project_id}")
-    fun deleteProject(@Header("Authorization") token: String,@Path("ProjectId")projectId: String) : Single<Response<Void>>
+    fun finishProject(
+        @Header("Authorization") token: String,
+        @Path("id") projectId: String,
+    ): Single<Response<Void>>
 
     @PATCH("/auth/image")
     fun changeProfileImage(
@@ -49,11 +55,24 @@ interface ProjectFlowAPI {
         @Body file: MultipartBody.Part,
     ): Single<Response<Void>>
 
+    @PATCH("/plan/close?project={projectId}&plan={planId}")
+    fun finishPlan(
+        @Header("Authorization") token: String,
+        @Part("projectId") projectId : String,
+        @Path("plan_id") PlanId: String,
+    ): Single<Response<Void>>
 
-
-
-
-
+    @Multipart
+    @POST("/project")
+    fun addProjectQuery(
+        @Header("Authorization") token: String,
+        @Query("projectName") projectName: String,
+        @Query("explanation") explanation:String,
+        @Query("startDate") startDate : String,
+        @Query("endDate") endDate : String,
+        @Part file: MultipartBody.Part,
+        @Query("emails") emails : Array<String>
+    ): Single<Response<Void>>
 
 
 
